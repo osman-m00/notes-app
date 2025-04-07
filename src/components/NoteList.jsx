57ad0cdc89b4fab2637ef84d24a2931cd19721d5
unknown <mohamedsr92@gmail.com>
2025-04-07 04:28:25 +0300

    delete and edit functionality implemented

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,22 +1,68 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { NotesContext } from "../context/NotesContext";
 
 const NoteList = () => {
-  const { notes } = useContext(NotesContext);
+  const { notes, deleteNote, editNote } = useContext(NotesContext);
+  const [editingId, setEditingId] = useState(null);
+  const [newText, setNewText] = useState("");
+
+  const editNoteUtility = (note) => {
+    setEditingId(note.id);
+    setNewText(note.text);
+  };
+
+  const handleEditEvent = (e) => {
+    setNewText(e.target.value);
+  };
+
+  const submitEdit = () => {
+    editNote(editingId, newText);
+    setEditingId(null);
+  };
 
   return (
     <div>
       {notes.map((note) => (
-        <div key={note.id}>
+        <div className="border mt-5 mb-5" key={note.id}>
           <h1>{note.id}</h1>
-          <p>{note.text}</p>
+
+          {/* Conditional rendering based on the id */}
+          {editingId === note.id ? (
+            <div>
+              <input
+                className="border"
+                type="text"
+                value={newText}
+                onChange={handleEditEvent}
+              />
+              <button onClick={submitEdit} className="border">
+                Save
+              </button>
+            </div>
+          ) : (
+            <p>{note.text}</p>
+          )}
+
           {/* Format and show the creation time of the note */}
           <small>
             {new Date(note.id).toLocaleTimeString([], {
-              hour: '2-digit',
-              minute: '2-digit',
+              hour: "2-digit",
+              minute: "2-digit",
             })}
           </small>
+
+          <div className="flex space-x-2">
+            <div className="border-2 w-20 flex justify-center items-center">
+              <button onClick={() => deleteNote(note.id)}>
+                Delete Note
+              </button>
+            </div>
+            <div className="border-2 w-20 flex justify-center items-center">
+              <button onClick={() => editNoteUtility(note)}>
+                Edit Note
+              </button>
+            </div>
+          </div>
         </div>
       ))}
     </div>
